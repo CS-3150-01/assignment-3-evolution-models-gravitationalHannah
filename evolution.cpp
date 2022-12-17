e53c958fd615b717628d1cb83985e1ff855a411d
gravitationalHannah <fallsee23@gmail.com>
2022-12-16 19:56:20 -0700

    thank you!

--- evolution.cpp
+++ evolution.cpp
@@ -33,12 +33,12 @@ class Object{
         void setDeath(float newDeathRotChance){
             deathRotChance = newDeathRotChance;
         }
-        bool trySpawn(){
+        bool trySpawn(int spawnChance){
             if(spawnChance == 0){
                 return false;
             }else return true;
         }
-        bool tryDeathRot(){
+        bool tryDeathRot(int deathRotChance){
             if(deathRotChance == 0){
                 return false;
             }else return true;
@@ -46,10 +46,9 @@ class Object{
 };
 
 class food: public Object, public Actions{
-    bool testR = tryDeathRot();
-    public:
-        void rot(){
-            if(testR){
+    private:
+        void rot(bool tryDeathRot){
+            if(tryDeathRot){
                 //code to remove food
                 foods--;
             }
@@ -58,39 +57,35 @@ class food: public Object, public Actions{
 };
 
 class creature: public Object, public Actions{
-    float reproduceChance = 0;
-    creature();
-    bool testD = tryDeathRot();
-    bool rep = tryReproduction();
-    public:
-        bool tryReproduction(){
+    float reproduceChance;
+    private:
+        bool tryReproduction(int reproduceChance){
             if(reproduceChance == 0){
                 return false;
             }else return true;
         }
-        void reproduction(){
-            if(rep){
+        void reproduction(bool reproduceChance){
+            if(reproduceChance){
                 createCreature();
             }
         }
-        void die(){
-            if(testD){
+        void die(bool tryDeathRot){
+            if(tryDeathRot){
                 creatures--;
             }
         }
 };
 
 class world: public Actions, public Object{
-    bool test = trySpawn();
     
     public:
-        void createCreature(){
-            if(test){
+        void createCreature(bool trySpawn){
+            if(trySpawn){
                 creatures++;
             }
         }
-        void createFood(){
-            if(test){
+        void createFood(bool trySpawn){
+            if(trySpawn){
                 foods++;
             }
         }
@@ -99,8 +94,10 @@ int main(){
     int N = 1;
     world w1;
     for(int i = 0; i<5; i++){
-    w1.setSpawn(rand() % N);
-    w1.createCreature();
+        int a = rand() % N;
+         w1.setSpawn(a);
+         bool test = w1.trySpawn(a);
+         w1.createCreature(test);
     }
    
     return 0;
